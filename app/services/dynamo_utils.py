ea9230f26f52797753de25d5dcf204f032c526bc
Frederic F. <17061560+freddo1503@users.noreply.github.com>
2025-03-23 21:12:06 +0100

    Refactor DynamoDB client and integration tests

--- app/services/dynamo_utils.py
+++ app/services/dynamo_utils.py
@@ -1,4 +1,6 @@
 import logging
+import typing as t
+from datetime import datetime
 
 import boto3
 from boto3.dynamodb.conditions import Attr
@@ -32,11 +34,11 @@ class DynamoDBClient:
             logger.error(f"Error adding item: {e.response['Error']['Message']}")
             return False
 
-    def update_item(self, key: dict, updates: dict):
-        expression = "SET " + ", ".join([f"{key} = :{key}" for key in updates.keys()])
-        expression_values = {f":{k}": v for k, v in updates.items()}
-
+    def update_item(self, key: dict, updates: dict) -> bool:
         try:
+            expression = "SET " + ", ".join(f"{k} = :{k}" for k in updates)
+            expression_values = {f":{k}": v for k, v in updates.items()}
+
             self.table.update_item(
                 Key=key,
                 UpdateExpression=expression,
@@ -74,11 +76,12 @@ class DynamoDBClient:
         date = law.lastUpdate.isoformat() if law.lastUpdate else None
 
         existing_item = self.get_item(key={"textId": text_id})
-        if existing_item:
+        if not existing_item:
+            item = {"textId": text_id, "date": date, "isProcessed": False}
+            self.put_item(item)
             return
-
-        item = {"textId": text_id, "date": date, "isProcessed": False}
-        self.put_item(item)
+        else:
+            return False
 
     def sync_new_loda_entries_to_dynamodb(
         self, api_client, payload: loda.RequestPayload
@@ -86,33 +89,48 @@ class DynamoDBClient:
         """
         Synchronizes multiple LODA entries to DynamoDB.
         """
+        logger.info("Starting LODA entries synchronization...")
         loda_response = fetch_loda_list(api_client, payload)
 
+        sync_count = 0
+
         for law in loda_response.results:
-            self.sync_new_law_to_dynamodb(law)
+            if self.sync_new_law_to_dynamodb(law):
+                sync_count += 1
 
-        return "Synchronization complete"
+        logger.info(f"Synchronized {sync_count} new laws successfully.")
+        return f"Synchronization complete: {sync_count} new laws added."
 
-    def get_last_unprocessed_law(self):
+    def get_last_unprocessed_law(self) -> t.Optional[t.Dict[str, t.Any]]:
         """
         Retrieves the latest unprocessed law entry (`isProcessed=false`) from DynamoDB.
-        Returns `textId` and `date` of the law.
+        Returns a dictionary with `textId` and `date` of the law.
+        Example format:
+        {'date': '2025-03-15T00:00:00+00:00', 'textId': 'LEGITEXT000051331433'}
         """
         try:
             response = self.table.scan(
-                FilterExpression=Attr("isProcessed").eq(
-                    False
-                ),  # Filter for unprocessed entries
-                ProjectionExpression="textId, date",  # Fetch only `textId` and `date`
+                FilterExpression=Attr("isProcessed").eq(False),
+                ProjectionExpression="#textId, #date",
+                ExpressionAttributeNames={
+                    "#textId": "textId",
+                    "#date": "date",
+                },
             )
             items = response.get("Items", [])
-            if items:
-                # Assuming latest entries have the highest `date`
-                last_unprocessed = sorted(items, key=lambda x: x["date"], reverse=True)[
-                    0
-                ]
-                return last_unprocessed
-            return None  # Return None if no unprocessed laws
+            if not items:
+                return None
+
+            last_unprocessed = sorted(items, key=lambda x: x["date"], reverse=True)[0]
+
+            date_str = last_unprocessed.get("date")
+            law_date = datetime.fromisoformat(date_str).date() if date_str else None
+
+            result = {
+                "date": law_date,
+                "textId": last_unprocessed.get("textId"),
+            }
+            return result
         except ClientError as e:
             logger.error(
                 f"Error retrieving last unprocessed law: {e.response['Error']['Message']}"
--- tests/integration/app/services/test_dynamo_utils.py
+++ tests/integration/app/services/test_dynamo_utils.py
@@ -1,17 +1,18 @@
-from datetime import date
+from datetime import date, datetime
 
-import boto3
-from models import loda
-from services.dynamo_utils import DynamoDBClient
-
+from app.models import loda
 from app.services.legifrance import fetch_loda_list
 
 
-def test_sync_new_loda_entries_to_dynamodb(api_client, table_name):
-    dynamodb = DynamoDBClient(table_name)
+def create_item(
+    text_id: str, timestamp: datetime = None, is_processed: bool = False
+) -> dict:
+    """Helper function to create a DynamoDB item."""
+    ts = (timestamp or datetime.utcnow()).isoformat()
+    return {"textId": text_id, "date": ts, "isProcessed": is_processed}
 
-    table = boto3.resource("dynamodb").Table(table_name)
 
+def test_sync_new_loda_entries_to_dynamodb(api_client, dynamodb_client, dynamo_table):
     request_payload = loda.RequestPayload(
         sort="PUBLICATION_DATE_DESC",
         legalStatus=["VIGUEUR"],
@@ -21,18 +22,85 @@ def test_sync_new_loda_entries_to_dynamodb(api_client, table_name):
         signatureDate=loda.DateRange(start=date(2024, 1, 1), end=date(2024, 2, 28)),
         pageSize=5,
     )
-
-    dynamodb.sync_new_loda_entries_to_dynamodb(
-        api_client=api_client, payload=request_payload
-    )
-
-    loda_response = fetch_loda_list(api_client=api_client, payload=request_payload)
-
-    for law in loda_response.results:
-        dynamo_response = table.get_item(Key={"textId": law.id})
-        assert "Item" in dynamo_response, (
-            f"Law with textId {law.id} not found in DynamoDB"
+    loda_response = None
+    try:
+        dynamodb_client.sync_new_loda_entries_to_dynamodb(
+            api_client=api_client, payload=request_payload
         )
+        loda_response = fetch_loda_list(api_client=api_client, payload=request_payload)
 
-        item = dynamo_response["Item"]
-        assert item["textId"] == law.id
+        for law in loda_response.results:
+            dynamo_response = dynamo_table.get_item(Key={"textId": law.id})
+            assert "Item" in dynamo_response, (
+                f"Law with textId {law.id} not found in DynamoDB"
+            )
+            item = dynamo_response["Item"]
+            assert item["textId"] == law.id
+    finally:
+        if loda_response:
+            for law in loda_response.results:
+                dynamo_table.delete_item(Key={"textId": law.id})
+
+
+def test_put_item(dynamodb_client, dynamo_table):
+    text_id = "test-id-put"
+    item = create_item(text_id)
+    try:
+        dynamodb_client.put_item(item)
+        response = dynamo_table.get_item(Key={"textId": text_id})
+
+        assert "Item" in response, f"Item with textId {text_id} not found in table"
+        assert response["Item"] == item
+    finally:
+        dynamo_table.delete_item(Key={"textId": text_id})
+
+
+def test_get_item(dynamodb_client, dynamo_table):
+    text_id = "test-id-get"
+    item = create_item(text_id)
+    try:
+        dynamo_table.put_item(Item=item)
+        retrieved_item = dynamodb_client.get_item(key={"textId": text_id})
+
+        assert retrieved_item == item, "Retrieved item does not match the original"
+    finally:
+        dynamo_table.delete_item(Key={"textId": text_id})
+
+
+def test_update_item(dynamodb_client, dynamo_table):
+    text_id = "test-id-update"
+    item = create_item(text_id)
+    try:
+        # Insert the initial item into the mock table
+        dynamo_table.put_item(Item=item)
+
+        # Define the update parameters
+        key = {"textId": text_id}
+        updates = {"isProcessed": True}
+
+        # Call the update_item method with the correct arguments
+        result = dynamodb_client.update_item(key=key, updates=updates)
+
+        # Assert that the update operation was successful
+        assert result is True, "Update operation did not return True"
+
+        # Retrieve the item and verify the updates
+        response = dynamo_table.get_item(Key=key)
+        updated_item = response.get("Item", {})
+
+        assert updated_item.get("isProcessed") is True, "Update operation failed"
+    finally:
+        # Cleanup: Delete the test item
+        dynamo_table.delete_item(Key={"textId": text_id})
+
+
+def test_delete_item(dynamodb_client, dynamo_table):
+    text_id = "test-id-delete"
+    item = create_item(text_id)
+    dynamo_table.put_item(Item=item)
+
+    dynamodb_client.delete_item(key={"textId": text_id})
+
+    response = dynamo_table.get_item(Key={"textId": text_id})
+
+    assert "Item" not in response, f"Item with textId {text_id} was not deleted"
