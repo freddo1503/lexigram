f199d0793bf22b1b8e9307d2069a45aab897ad78
Frederic F. <17061560+freddo1503@users.noreply.github.com>
2025-03-24 15:10:51 +0100

    Add Secrets Manager integration for .env file handling
    Introduced a new construct to load environment variables from a .env file and store them securely in AWS Secrets Manager. This enhances security by centralizing sensitive information management within Secrets Manager.

--- infra/lexigram_stack.py
+++ infra/lexigram_stack.py
@@ -1,9 +1,14 @@
-from aws_cdk import Stack
+import json
+from pathlib import Path
+
+import aws_cdk
+import boto3
 from constructs import Construct
+from dotenv import dotenv_values
 from dynamo_db_table import LawPostsDynamoDBTable
 
 
-class Lexigram(Stack):
+class Lexigram(aws_cdk.Stack):
     def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
@@ -11,3 +16,32 @@ class Lexigram(Stack):
             self,
             "LawPostsDynamoDBTable",
         )
+
+        SecretsManagerConstruct(
+            self, "LexigramEnvSecrets", secret_name="my-env-secrets"
+        )
+
+
+class SecretsManagerConstruct(Construct):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        env_file_path: Path = Path(".env"),
+        secret_name: str = "my-env-secrets",
+    ):
+        super().__init__(scope, id)
+        env_vars = dotenv_values(env_file_path)
+        secret_value = json.dumps(env_vars)
+        self.create_or_update_secret(secret_name, secret_value)
+
+    def create_or_update_secret(self, secret_name: str, secret_value: str):
+        client = boto3.client("secretsmanager")
+
+        try:
+            client.describe_secret(SecretId=secret_name)
+            client.put_secret_value(SecretId=secret_name, SecretString=secret_value)
+        except client.exceptions.ResourceNotFoundException:
+            client.create_secret(Name=secret_name, SecretString=secret_value)
+        except Exception as e:
+            print(f"An error occurred while managing the secret: {e}")
